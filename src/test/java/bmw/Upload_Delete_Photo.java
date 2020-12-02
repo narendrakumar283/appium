26d04e86ac83fe5533df284c4e0130a54c230b86
narendrakumar283 <72879500+narendrakumar283@users.noreply.github.com>
2020-12-02 23:03:33 +0530

    Update Upload_Delete_Photo.java

--- src/test/java/bmw/Upload_Delete_Photo.java
+++ src/test/java/bmw/Upload_Delete_Photo.java
@@ -21,7 +21,7 @@ public class Upload_Delete_Photo
 		//start appium server
 		Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /k \"appium --relaxed-security\"");
 		URL u=new URL("http://0.0.0.0:4723/wd/hub");
-		//Define desired capabilities related to device and VodQA app
+		//Define desired capabilities related to device and app
 		DesiredCapabilities dc=new DesiredCapabilities();
 		dc.setCapability(CapabilityType.BROWSER_NAME,"");
 		dc.setCapability("deviceName","emulator-5554");
@@ -59,9 +59,9 @@ public class Upload_Delete_Photo
 			By onoff=By.xpath("//*[@text='Off' or @text='On']");
 			WebDriverWait wait=new WebDriverWait(driver,20);
 			WebDriverWait shortWait=new WebDriverWait(driver,5);
-		    wait.until(ExpectedConditions.presenceOfElementLocated(backupSwitch)).click();
-		    wait.until(ExpectedConditions.presenceOfElementLocated(touchOutside)).click();
-		    wait.until(ExpectedConditions.presenceOfElementLocated(keepoff)).click();
+		        wait.until(ExpectedConditions.presenceOfElementLocated(backupSwitch)).click();
+		        wait.until(ExpectedConditions.presenceOfElementLocated(touchOutside)).click();
+		        wait.until(ExpectedConditions.presenceOfElementLocated(keepoff)).click();
 		    //delete all existing pictures via an infinite loop,which can terminate when no more pictures
 		    try
 		    {
@@ -78,16 +78,16 @@ public class Upload_Delete_Photo
 		    }
 		    Thread.sleep(5000);
 
-		File img=new File("D:\\batch249\\appiumexamples\\volume.png");
-		driver.pushFile("/mnt/sdcard/Pictures/"+img.getName(),img);
-		Thread.sleep(5000);
-		ExpectedCondition condition=ExpectedConditions.numberOfElementsToBe(photo,1);
-		wait.until(condition);
-		System.out.println("Given pic uploaded successfully");
-		wait.until(ExpectedConditions.presenceOfElementLocated(menu)).click();
-		wait.until(ExpectedConditions.presenceOfElementLocated(settings)).click();
-	    wait.until(ExpectedConditions.presenceOfElementLocated(bo)).click();
-		wait.until(ExpectedConditions.presenceOfElementLocated(onoff)).click();
+		    File img=new File("D:\\batch249\\appiumexamples\\volume.png");
+		    driver.pushFile("/mnt/sdcard/Pictures/"+img.getName(),img);
+		    Thread.sleep(5000);
+		    ExpectedCondition condition=ExpectedConditions.numberOfElementsToBe(photo,1);
+		    wait.until(condition);
+		    System.out.println("Given pic uploaded successfully");
+		    wait.until(ExpectedConditions.presenceOfElementLocated(menu)).click();
+		    wait.until(ExpectedConditions.presenceOfElementLocated(settings)).click();
+	            wait.until(ExpectedConditions.presenceOfElementLocated(bo)).click();
+		    wait.until(ExpectedConditions.presenceOfElementLocated(onoff)).click();
 		}
 		catch(Exception ex)
 		{
